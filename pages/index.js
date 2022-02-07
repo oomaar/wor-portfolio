3d70c4e291577dc4803860faeeff148423272ae3
oomaar <omaar_5@hotmail.com>
2022-02-07 12:50:28 +0200

    define Main styles

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import { Hero } from "../components";
+import { About, Hero } from "../components";
 import { Head } from "../global/Head";
 import websiteData from "../data/websiteData.json";
+import styled from 'styled-components';
+
+const Main = styled.main`
+  margin-top: 32rem;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 25rem;
+  }
+`;
 
 export default function Home() {
   return (
@@ -8,6 +17,9 @@ export default function Home() {
       <Head title="Worex Landing Page" />
 
       <Hero data={websiteData.hero} />
+      <Main>
+        <About />
+      </Main>
     </div>
   );
 };
